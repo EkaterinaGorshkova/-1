4ef0cee872543487a74bd2c153eb19e172ea7822
ekaterina <sgorskovaekaterina@gmail.com>
2022-10-05 13:09:17 +0500

    напсала программу

---
+++ проверочная.cs
@@ -0,0 +1,21 @@
+﻿string[] startArray = new string[8] { "85214", "1521", "425", "852", "521478", "41", "521", "552" };
+string[] finalArray = new string[startArray.Length];
+
+void NewArray(string[] firstArray, string[] secondArray)
+{
+    int count = 0;
+    for (int i = 0; i < firstArray.Length; i++)
+    {
+        if (firstArray[i].Length <= 3)
+        {
+            secondArray[count] = firstArray[i];
+            count++;
+        }
+    }
+}
+void PrintArray(string[] array){
+    Console.WriteLine($"Array : {String.Join(" ", array)}");
+}
+NewArray(startArray,finalArray);
+PrintArray(startArray);
+PrintArray(finalArray);
